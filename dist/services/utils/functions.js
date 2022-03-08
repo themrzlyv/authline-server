a57003f7443670aef3f75f13c332e8d503e2bb25
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-03-09 00:00:48 +0400

    deleted dist file

--- dist/services/utils/functions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateEmail = void 0;
-const validateEmail = (email) => {
-    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-};
-exports.validateEmail = validateEmail;
