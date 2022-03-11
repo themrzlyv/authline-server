63f120720ab28324abd6f214b2b141c4c7c25b5d
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-03-11 16:50:02 +0400

    deleted

--- app/middlewares/errorHandler.ts
+++ app/middlewares/errorHandler.ts
@@ -3,10 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 
 const errorHandler = async (error: any, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof ApiError) {
-    return res.status(error.code).send({ error: error.message });
+    return res.status(error.code).json({ error: error.message });
   }
 
-  return res.status(500).send({ error: 'Internal server error!' });
+  return res.status(500).json({ error: 'Internal server error!' });
 };
 
 export default errorHandler;
