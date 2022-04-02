596218223970a0f7b98037edec10d1f59c9c6abc
Samir Mirzaliyev <themirzaliyev@Samirs-MacBook-Air.local>
2022-04-02 23:15:18 +0400

    cookie

--- app/app.ts
+++ app/app.ts
@@ -21,7 +21,7 @@ const startServer = async (): Promise<void> => {
   // middlewares
   app.use(cookieParser());
   app.set('trust proxy', 1);
-  app.use(cors({ origin: true, credentials: true }));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
